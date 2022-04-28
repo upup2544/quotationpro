15b3a36c85bb20d0df258b5ec1a880ee3df1baf5
upup2544 <upup2544@hotmail.com>
2022-04-28 12:06:37 +0700

    ok

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { Router, Route } from "svelte-routing";
+	import Home from "./pages/Home.svelte";
+	import SideNav from "./layouts/SideNav.svelte";
+
+	export let url = "";
+</script>
+
+<Router {url}>
+	<main>
+		<SideNav />
+		<div>
+			<Route path="/" component={Home} />
+		</div>
+	</main>
+</Router>
+
+<style>
+	main {
+		display: grid;
+		grid-template-columns: 240px 1fr;
+		gap: 1em;
+	}
+</style>
---
+++ src/layouts/SideNav.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { navigate } from "svelte-routing";
+
+    function createHandleLinkClick(link) {
+        return () => {
+            navigate(link);
+        };
+    }
+
+    const navs = [
+        ["/home", "Home"],
+        ["/quotation", "Quotation"],
+        ["/customers", "Customers"],
+        ["/web-settings", "Web Settings"],
+        ["/archive", "Archive"]
+    ];
+</script>
+
+<main>
+    <div class="sidenav">
+        {#each navs as [link, text]}
+            <div on:click={createHandleLinkClick(link)}>{text}</div>
+        {/each}
+    </div>
+</main>
+
+<style>
+    .sidenav {
+        height: 100%; /* 100% Full-height */
+        width: 8%; /* 0 width - change this with JavaScript */
+        position: fixed; /* Stay in place */
+        z-index: 1; /* Stay on top */
+        top: 0; /* Stay at the top */
+        left: 0;
+        background-color: rgb(14, 9, 9); /* Black*/
+        /* Disable horizontal scroll */
+        padding-top: 60px; /* Place content 60px from the top */
+        transition: 0.5s; /* 0.5 second transition effect to slide in the sidenav */
+        background-color: white;
+    }
+</style>
